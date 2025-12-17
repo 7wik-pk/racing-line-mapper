9fa272285c42a69822f074520adaba99f1c519e4
Sathwik P. Kallapur <satpk17@gmail.com>
2025-12-17 16:09:39 +1100

    add prelim image processing

--- cmd/debug-mesh/debug_preproc.go
+++ cmd/debug-mesh/debug_preproc.go
@@ -1,38 +1,41 @@
 package main
 
-// import (
-// 	"fmt"
-// 	"gocv.io/x/gocv"
-// )
+import (
+	"fmt"
+	"gocv.io/x/gocv"
+)
 
-// func main(){
-// 	// 1. Load the image
-// 	img := gocv.IMRead("input.jpg", gocv.IMReadColor)
-// 	if img.Empty() {
-// 		fmt.Printf("Error reading image\n")
-// 		return
-// 	}
-// 	defer img.Close()
+func main(){
 
-// 	// 2. Convert to grayscale
-// 	gray := gocv.NewMat()
-// 	defer gray.Close()
-// 	gocv.CvtColor(img, &gray, gocv.ColorBGRToGray)
+	fmt.Println("running debug preproc script...")
 
-// 	// 3. Apply Canny edge detection
-// 	// Define thresholds (these often require tuning for different images)
-// 	// Weak edges connected to strong edges are kept (hysteresis thresholding)
-// 	lowThreshold := float32(50)
-// 	highThreshold := float32(150)
-// 	cannyOutput := gocv.NewMat()
-// 	defer cannyOutput.Close()
+	// 1. Load the image
+	img := gocv.IMRead("./input_track_maps/test.jpg", gocv.IMReadColor)
+	if img.Empty() {
+		fmt.Printf("Error reading image\n")
+		return
+	}
+	defer img.Close()
+
+	// 2. Convert to grayscale
+	gray := gocv.NewMat()
+	defer gray.Close()
+	gocv.CvtColor(img, &gray, gocv.ColorBGRToGray)
+
+	// 3. Apply Canny edge detection
+	// Define thresholds (these often require tuning for different images)
+	// Weak edges connected to strong edges are kept (hysteresis thresholding)
+	lowThreshold := float32(50)
+	highThreshold := float32(150)
+	cannyOutput := gocv.NewMat()
+	defer cannyOutput.Close()
 	
-// 	// The third parameter is the aperture size for the Sobel operator (e.g., 3, 5, or 7)
-// 	gocv.Canny(gray, &cannyOutput, lowThreshold, highThreshold)
+	// The third parameter is the aperture size for the Sobel operator (e.g., 3, 5, or 7)
+	gocv.Canny(gray, &cannyOutput, lowThreshold, highThreshold)
 
-// 	// 4. Save the result
-// 	if ok := gocv.IMWrite("output_edges.jpg", cannyOutput); !ok {
-// 		fmt.Printf("Error writing image\n")
-// 	}
-// 	fmt.Printf("Canny edge detection applied and saved to output_edges.jpg\n")
-// }
+	// 4. Save the result
+	if ok := gocv.IMWrite("output_edges.jpg", cannyOutput); !ok {
+		fmt.Printf("Error writing image\n")
+	}
+	fmt.Printf("Canny edge detection applied and saved to output_edges.jpg\n")
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.25.4
 
 require (
 	github.com/hajimehoshi/ebiten/v2 v2.9.4
+	gocv.io/x/gocv v0.42.0
 // gocv.io/x/gocv v0.42.0
 )
 
